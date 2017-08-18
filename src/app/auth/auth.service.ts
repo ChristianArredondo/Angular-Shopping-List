96159da6b3e3703499fb7e555469d7ac6cad11c7
christianarredondo <christianarredondo@utexas.edu>
2017-08-17 22:01:05 -0500

    add homepage route on login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Route, Router, ActivatedRoute } from '@angular/router';
+
 
 @Injectable()
 export class AuthService {
   token: string;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) {}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -19,6 +25,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
             .then(
               (token: string) => this.token = token
@@ -29,10 +36,12 @@ export class AuthService {
         error => console.log(error)
       )
   }
+
   logoutUser () {
     firebase.auth().signOut();
     this.token = null;
   }
+
   getToken () {
     firebase.auth().currentUser.getIdToken()
       .then(
@@ -44,4 +53,8 @@ export class AuthService {
   isAuthenticated () {
     return this.token != null;
   }
+
+  // onSuccess () {
+  //   this.router.navigate(['edit'], {relativeTo: this.route});
+  // }
 }
