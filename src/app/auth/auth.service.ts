c8ffc7a655301fe0e49af4bc8a56ca065df1e243
christianarredondo <christianarredondo@utexas.edu>
2017-08-27 12:41:53 -0500

    add actions for user Signup, Login, and Logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
-
+import { Store } from '@ngrx/store';
+import * as fromAuth from './store/auth.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Injectable()
 export class AuthService {
   token: string;
 
   constructor(
-    private router: Router) {}
+    private router: Router,
+    private store: Store<fromAuth.State>
+  ) {}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(
-        () => console.log('Creating new user')
+        response => [
+          this.store.dispatch(new AuthActions.Signup())
+        ]
       )
       .catch(
         error => console.log(error)
@@ -24,6 +30,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.store.dispatch(new AuthActions.Login())
           this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
             .then(
@@ -38,6 +45,7 @@ export class AuthService {
 
   logoutUser () {
     firebase.auth().signOut();
+    this.store.dispatch(new AuthActions.Logout())
     this.token = null;
   }
 
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,11 @@
-import { Component, Output, Injectable } from '@angular/core';
+import { Component, Output, Injectable, OnInit } from '@angular/core';
 import { DataStorageService } from '../../shared';
 import { Headers, Http, Response } from '@angular/http/';
 import { AuthService } from '../../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromAppState from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducer';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 @Component({
@@ -11,13 +15,18 @@ import { AuthService } from '../../auth/auth.service';
 
 })
 
-
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  authState: Observable<fromAuth.State>
 
   constructor(
     private dataStorageService: DataStorageService,
-    public authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromAppState.AppState>
   ) {}
+
+  ngOnInit () {
+    this.authState = this.store.select('auth');
+  }
 
   onStoreRecipes () {
     this.dataStorageService.storeRecipes()
